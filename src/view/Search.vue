2c3ee85ce041ab74e41e56a5be7bd35367c0bc65
LaoHu97 <962271461@qq.com>
2017-10-31 09:07:19 +0800

    新增美剧频道

---
+++ src/view/Search.vue
@@ -0,0 +1,35 @@
+<template>
+  <div class="page-search">
+    <mt-field label="搜索" placeholder="请输入电影名称" v-model="username">&nbsp;<mt-button size="normal" type="primary" style="height:35px;">确定</mt-button></mt-field>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      value: '13',
+      result:[]
+    };
+  },
+  computed: {
+
+  },
+  methods: {
+
+  },
+
+};
+</script>
+
+<style lang="css">
+  .page-search {
+    height: 100%;
+  }
+  .page-search a{
+    background-color: #D3DCE6;
+  }
+  .mint-field-core{
+    background-color: #D3DCE6;
+  }
+</style>
